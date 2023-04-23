24fd385781fb60d35ea4cbb15f6168316d904bca
wangli <wl1337076504@163.com>
2023-04-23 16:29:08 +0800

    img

--- src/service/getConfig.js
+++ src/service/getConfig.js
@@ -1,27 +1,20 @@
 import axios from 'axios';
-
+import cg_resource from '/public/cg_resource.json'
 let CGUtils = {}
 
 CGUtils.getImgList = () => {
     return new Promise((resolve, reject) => {
-        axios.get(window.config.pathPre + `/数据CG/cg_resource.json?v=${new Date().getTime()}`).then(res => {
-            console.log('res=', res)
-            if(typeof res.data === "string") {
-                res.data = JSON.parse(res.data)
-            }
-            res.data.forEach(item => {
-                item.path = window.config.pathPre + '/数据CG/CG资源图库/' + item.fileName
-            })
-            resolve(res.data)
-        }).catch(e => {
-            reject(e)
+        let list = cg_resource
+        list.forEach(item => {
+            item.path = '/数据CG/CG资源图库/' + item.fileName
         })
+        resolve(list)
     })
 }
 CGUtils.addPreUrl = (imgList) => {
     let result = []
     imgList.forEach(item => {
-        let temp = window.config.pathPre + '/数据CG/CG资源图库/' + item
+        let temp = window.config.pathPre + '/public/数据CG/CG资源图库/' + item
         result.push(temp)
     })
     return result
