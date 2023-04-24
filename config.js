59957d01ff72c181f9f1dc22a920b962786d133b
DivingEagleWang <37290889+DivingEagleWang@users.noreply.github.com>
2023-04-24 12:54:04 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var setConfigData = (function () {
     window.config = {
-        // pathPre: '',
-        pathPre: 'http://' + window.location.host,
+        pathPre: 'http://rtlrswo4e.hn-bkt.clouddn.com',
+        // pathPre: 'http://' + window.location.host,
         publicPre: '/数据CG/CG资源图库/',
         filePath: '/数据CG/CG资源图库',
         updateTime: '2023.4.22更新'
